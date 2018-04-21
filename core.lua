c1cdc977f50ddcfe62229e947e45e3d15c449d82
absolitudo <absolitudo@gmail.com>
2018-04-21 19:49:23 +0200

    add basic slash command for toggling the visibility of the mainframe

--- core.lua
+++ core.lua
@@ -1,2 +1,11 @@
 local ns = (select(2, ...))
 ns.frames = {}
+
+SLASH_AUCPROFITCALC1 = '/apc'
+SlashCmdList['AUCPROFITCALC'] = function(...)
+    if not ns.frames.mainFrame:IsShown() then
+        ns.frames.mainFrame:Show()
+    else
+        ns.frames.mainFrame:Hide()
+    end
+end
