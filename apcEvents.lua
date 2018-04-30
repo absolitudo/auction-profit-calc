5f5b465927148e1802433266d1fd21aac67ee850
absolitudo <absolitudo@gmail.com>
2018-04-30 19:37:03 +0200

    on blizzard trade ui load, hook a function to recipe selection

--- apcEvents.lua
+++ apcEvents.lua
@@ -1,13 +1,8 @@
 local ns = (select(2, ...))
 local APC = ns
 
-local function APCTradeSkillUpdate()
-
-end
-
 local function APCtradeSkillEventsHandler(self, event, ...)
-    print(...)
-    if(event == 'TRADE_SKILL_SHOW') then
+    if event == 'TRADE_SKILL_SHOW' then
         if(not APC.frames.mainFrame:IsVisible()) then
             APC.frames.mainFrame:ShowFrame()
         end
@@ -15,15 +10,20 @@ local function APCtradeSkillEventsHandler(self, event, ...)
         if(APC.frames.mainFrame:IsVisible()) then
             APC.frames.mainFrame:Hide()
         end
-    else
-        APCTradeSkillUpdate()
+    elseif event == 'ADDON_LOADED' and ... == 'Blizzard_TradeSkillUI' then
+        APCtradeSkillEvents:UnregisterEvent('ADDON_LOADED')
+        hooksecurefunc("TradeSkillFrame_SetSelection",
+            function(id) 
+                print(id)
+            end
+        )
     end
 end
 
 function APC:registerEvents()
     local APCtradeSkillEvents = CreateFrame("FRAME", "APCTradeSkillEvent")
     APCtradeSkillEvents:RegisterEvent("TRADE_SKILL_SHOW")
-    APCtradeSkillEvents:RegisterEvent("TRADE_SKILL_UPDATE")
     APCtradeSkillEvents:RegisterEvent("TRADE_SKILL_CLOSE")
+    APCtradeSkillEvents:RegisterEvent("ADDON_LOADED")
     APCtradeSkillEvents:SetScript("OnEvent", APCtradeSkillEventsHandler)
 end
--- core.lua
+++ core.lua
@@ -4,52 +4,52 @@ local APC = ns
 APC.frames:initFrames()
 APC:registerEvents()
 
-APC.scanData = {}
-APC.professions = {}
+--APC.scanData = {}
+--APC.professions = {}
 
-prof1, prof2, _, _, cooking = GetProfessions()
-APC.professions[1] = function() return GetProfessionInfo(prof1) end
-APC.professions[2] = function() return GetProfessionInfo(prof2) end
+--prof1, prof2, _, _, cooking = GetProfessions()
+--APC.professions[1] = function() return GetProfessionInfo(prof1) end
+--APC.professions[2] = function() return GetProfessionInfo(prof2) end
 
 --ns.professions['cooking'] = GetProfessionInfo(cooking)
 
 
 
 
-local function logTable(table)
-    for key, value in pairs(table) do
-        print(key, value)
-        if type(value) == 'table' then
-            logTable(value)
-            print('---')
-        end
-    end
-end
+--local function logTable(table)
+--   for key, value in pairs(table) do
+--        print(key, value)
+--        if type(value) == 'table' then
+--            logTable(value)
+--            print('---')
+--       end
+--    end
+--end
 --ns.frames.initFrames()
 -- _G.AucAdvanced.GetFaction this is the server key
 -- _G.AucAdvanced.Modules.Util.ScanData.GetScanData this is the scan data requires serverkey
-SLASH_AUCPROFITCALC1 = '/apc'
-SlashCmdList['AUCPROFITCALC'] = function(...)
-    local params = ...
-    if params == '' then
-        print(TradeSkillFrame:GetWidth())
-        print(TradeSkillFrame:GetHeight())
+--SLASH_AUCPROFITCALC1 = '/apc'
+--SlashCmdList['AUCPROFITCALC'] = function(...)
+--    local params = ...
+--   if params == '' then
+--       print(TradeSkillFrame:GetWidth())
+--       print(TradeSkillFrame:GetHeight())
         
-    end
-    if params == 'load' then
+--   end
+--    if params == 'load' then
         --print('loading scan data')
         --_G.AucAdvanced.Scan.LoadScanData()
         --ns.scanData = _G.AucAdvanced.Modules.Util.ScanData.GetScanData(_G.AucAdvanced.GetFaction())
         
-        print(APC.professions[1]())
-        print(APC.professions[2]())
+--        print(APC.professions[1]())
+--        print(APC.professions[2]())
         
-    end
+--    end
 
     --if not ns.frames.mainFrame:IsShown() then
     --    ns.frames.mainFrame:Show()
     --else
     --    ns.frames.mainFrame:Hide()
     --end
-end
+--end
 
