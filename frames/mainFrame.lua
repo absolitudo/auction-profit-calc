9057ca5100d65946357b3164da5878b1ae0a5761
absolitudo <absolitudo@gmail.com>
2018-05-03 03:52:53 +0200

    add title for mainframe

--- frames/mainFrame.lua
+++ frames/mainFrame.lua
@@ -9,11 +9,17 @@ function APC.frames.initFrames()
 end
 
 function APC.frames.mainFrame:initFrame()
+    -- Frame 
     APC.frames.mainFrame = CreateFrame("Frame", "AucProfitCalc", UIParent, 'BasicFrameTemplate')
     APC.frames.mainFrame:SetMovable(true)
     APC.frames.mainFrame:SetWidth(338)
     APC.frames.mainFrame:SetHeight(424)
     APC.frames.mainFrame:Hide()
+
+    APC.frames.mainFrame.title = APC.frames.mainFrame:CreateFontString('MainFrameTitle')
+    APC.frames.mainFrame.title:SetFontObject('GameFontHighlight')
+    APC.frames.mainFrame.title:SetText('Auction profit calculator')
+    APC.frames.mainFrame.title:SetPoint('TOP', APC.frames.mainFrame, 'TOP', 0, -5)
 
     function APC.frames.mainFrame:ShowFrame()
         APC.frames.mainFrame:SetPoint("TOPLEFT", TradeSkillFrame, "TOPRIGHT")
